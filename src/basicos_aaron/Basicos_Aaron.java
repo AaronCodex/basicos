17048e8164fb243aa566d40d0a727eeeff169c11
Aarón Moreno <aaron.severoochoa@gmail.com>
2019-11-18 13:37:55 +0100

    añadimos el metodo cadenas

--- src/basicos_aaron/Basicos_Aaron.java
+++ src/basicos_aaron/Basicos_Aaron.java
@@ -10,12 +10,17 @@ package basicos_aaron;
  * @author Aarón Moreno
  */
 public class Basicos_Aaron {
+    public static void cadenas() {
+        String x="Hola, soy Aarón";
+        System.out.println(x);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        cadenas();
     }
     
 }
