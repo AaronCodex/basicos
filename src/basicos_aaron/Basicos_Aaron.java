ed1de900b760bab78236622c8f22656e42c929b3
Aarón Moreno <aaron.severoochoa@gmail.com>
2019-11-18 13:38:40 +0100

    añadimos el metodo logicos

--- src/basicos_aaron/Basicos_Aaron.java
+++ src/basicos_aaron/Basicos_Aaron.java
@@ -10,12 +10,21 @@ package basicos_aaron;
  * @author Aarón Moreno
  */
 public class Basicos_Aaron {
+    public static void logicos() {
+        boolean x=false;
+        int a=3, b=2;
+        if(a>b){
+            x=true;
+            System.out.println(a+" es mayor que "+b+": "+x);
+        } else System.out.println(a+" es menor que "+b+": "+x);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        logicos();
     }
     
 }
